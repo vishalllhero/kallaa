ab40812bdcc4afcc1ffa4159aae7a43df2ac3ff9
virtuallifecoinsol <vishalpandaya552@gmail.com>
2026-04-23 14:50:45 +0530

    fix tailwind production issue

--- server/src/models/Product.ts
+++ server/src/models/Product.ts
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String, default: "" },
     price: { type: Number, required: true },
     image: { type: String, required: true },
-    story: { type: String, required: true },
+    story: { type: String, default: "" },
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
